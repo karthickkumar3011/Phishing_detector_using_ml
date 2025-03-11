1129c0cfd362a56f0acec359f3c45193e34bc432
karthickkumar3011 <karthikumar3011@gmail.com>
2025-03-11 12:22:57 +0530

    Developed one more model(Naive Bayes)

--- app.py
+++ app.py
@@ -4,16 +4,20 @@ import joblib  # For loading the models
 import numpy as np
 from utils.features_extraction import extract_features  # Import the function
 import tensorflow as tf  # Make sure to import TensorFlow
+import warnings
+
+warnings.filterwarnings("ignore")
 
 app = Flask(__name__)
 
 # Load your models
-rf_model = joblib.load('saved_models/Random_Forest.pkl')
-xgb_model = joblib.load('saved_models/XGBoost.pkl')
-log_reg_model=joblib.load('saved_models\LogisticRegression.pkl')
-decision_tree_model=joblib.load('saved_models\DecisionTree.pkl')
-knn_model=joblib.load('saved_models\KNN.pkl')
-svm_model=joblib.load('saved_models\LogisticRegression.pkl')
+rf_model = joblib.load(r'saved_models/Random_Forest.pkl')
+xgb_model = joblib.load(r'saved_models/XGBoost.pkl')
+log_reg_model=joblib.load(r'saved_models/LogisticRegression.pkl')
+decision_tree_model=joblib.load(r'saved_models/DecisionTree.pkl')
+knn_model=joblib.load(r'saved_models/KNN.pkl')
+svm_model=joblib.load(r'saved_models/LogisticRegression.pkl')
+nb_model=joblib.load(r'saved_models/Naive_Bayes.pkl')
 
 @app.route('/')
 def home():
@@ -49,6 +53,7 @@ def predict():
     decision_tree_prediction = decision_tree_model.predict(feature_array)
     knn_prediction = knn_model.predict(feature_array)
     svm_prediction = svm_model.predict(feature_array)
+    nb_prediction = nb_model.predict(feature_array)
 
     # Interpret the predictions
     rf_result = "Phishing" if rf_prediction[0] == 0 else "Legitimate"
@@ -57,6 +62,7 @@ def predict():
     decision_tree_result = "Phishing" if decision_tree_prediction[0] == 0 else "Legitimate"
     knn_result = "Phishing" if knn_prediction[0] == 0 else "Legitimate"
     svm_result = "Phishing" if svm_prediction[0] == 0 else "Legitimate"
+    nb_result = "Phishing" if nb_prediction[0] == 0 else "Legitimate"
 
     return render_template('index.html', 
                            prediction_rf=rf_result, 
@@ -64,7 +70,8 @@ def predict():
                            prediction_log_reg=log_reg_result,
                            prediction_decision_tree=decision_tree_result,
                            prediction_knn=knn_result,
-                           prediction_svm=svm_result
+                           prediction_svm=svm_result,
+                           prediction_nb=nb_result
                            )
 
 if __name__ == '__main__':
